db50a5cb7a464e7c425573f9023afacf2ef15e53
Sunyanan Choochotkaew <sunyanan.choochotkaew1@ibm.com>
2023-01-26 11:40:45 +0900

    add aws-ipvlan cni to daemon build
    Signed-off-by: Sunyanan Choochotkaew <sunyanan.choochotkaew1@ibm.com>

--- daemon/run.sh
+++ daemon/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cp /usr/local/app/multi-nic /host/opt/cni/bin/multi-nic
 cp /usr/local/app/multi-nic-ipam /host/opt/cni/bin/multi-nic-ipam
+cp /usr/local/app/aws-ipvlan /host/opt/cni/bin/aws-ipvlan
 ./daemon
