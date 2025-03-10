688e0cca78db450bfea4cd17d58cdcbafc9421bf
Sunyanan Choochotkaew <sunyanan.choochotkaew1@ibm.com>
2025-03-10 19:52:29 +0900

    chore: upgrade go and CNI for daemon
    Signed-off-by: Sunyanan Choochotkaew <sunyanan.choochotkaew1@ibm.com>

--- daemon/run.sh
+++ daemon/run.sh
@@ -2,4 +2,5 @@
 cp /usr/local/app/multi-nic /host/opt/cni/bin/multi-nic
 cp /usr/local/app/multi-nic-ipam /host/opt/cni/bin/multi-nic-ipam
 cp /usr/local/app/aws-ipvlan /host/opt/cni/bin/aws-ipvlan
+echo "Starting multi-nicd"
 ./daemon
